e7f611a44fa8bd419497503a263d5aac4b304003
zelixpcapfiles <usazelix@gmail.com>
2026-04-11 02:28:17 -0700

    Update alert message for desktop mode detection

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ updateLoadingScreenTheme();
   let devOpen=false;
   setInterval(()=>{
     if(window.outerWidth-window.innerWidth>160||window.outerHeight-window.innerHeight>160){
-      if(!devOpen){devOpen=true;alert('Matikan Mode desktop / inspeksi.');}
+      if(!devOpen){devOpen=true;alert('Matikan Mode desktop / inspeksi / pop up blocker.');}
     } else devOpen=false;
   },1500);
   document.addEventListener('selectstart',e=>e.preventDefault());
